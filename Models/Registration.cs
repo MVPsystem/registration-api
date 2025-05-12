9c4986323fed8b70a2c275b3c2f119d2955dc07d
Aden <outdoor.co.se@gmail.com>
2025-05-12 10:50:48 +0200

    Initial commit for Registration API

---
+++ Models/Registration.cs
@@ -0,0 +1,10 @@
+﻿namespace RegistrationApi.Models
+{
+    public class Registration
+    {
+        public int Id { get; set; }
+        public string ParticipantName { get; set; }
+        public string Email { get; set; }
+        public int EventId { get; set; }
+    }
+}
